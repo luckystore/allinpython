f35c7c7066648d67cd0b1160809ef8647412cba5
MKTECHPARK <kiranmekanuri@gmail.com>
2018-07-27 19:33:17 +0530

    CreditNotifier Producer and Consumer Codes

--- com/CreditNotifier/CreditFeedConsumer.py
+++ com/CreditNotifier/CreditFeedConsumer.py
@@ -1,6 +1,37 @@
 from kafka import KafkaConsumer
+# import time
+import pymysql
+from datetime import datetime
 
 consumer = KafkaConsumer('FeedTopic')
+# redisserver = redis.Redis()
+
+
+conn = pymysql.connect(host='localhost', user='root', password='password', db='mysql')
+a = conn.cursor()
+add_temperature_sql = ("""
+                            insert into mysql.credit_feeds
+                            (FI, ACCOUNT_TYPE, CUSTOMER_ID, ACCOUNT_NO) 
+                            values(%s, %s, %s, %s)
+
+                            """)
+
 for msg in consumer:
-    kafka_msg = msg.value.decode('utf-8')
-    print(kafka_msg)
+    try:
+        print(msg.value)
+        messageByte = msg.value.decode("utf-8")
+        print(type(messageByte))
+        print(messageByte.split(',')[1])
+        print(type(messageByte.split(',')[4]))
+        print(messageByte.split(',')[4])
+        fi = messageByte.split(',')[1]
+        account_type = messageByte.split(',')[2]
+        customer_id = messageByte.split(',')[3]
+        account_no = messageByte.split(',')[4]
+
+        a.execute(add_temperature_sql, (fi, account_type, customer_id, account_no))
+        conn.commit()
+    except:
+        conn.rollback()
+
+conn.close()
--- com/CreditNotifier/CreditFeedProducer.py
+++ com/CreditNotifier/CreditFeedProducer.py
@@ -1,6 +1,6 @@
 from kafka import KafkaProducer
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
-for _ in range(100):
-    producer.send('FeedTopic', b'I am stored in db')
+#for _ in range(100):
+producer.send('FeedTopic', b'1, WellsForgo, Savings, 1, 3323232324, John, $750,1532490000000,1532320000000,ECS')
     # This is important
 producer.flush()
